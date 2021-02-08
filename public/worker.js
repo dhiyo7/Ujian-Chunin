9063d8c91daec59c4e659f72db54360d4f7f3894
dhiyo7 <dhiyoreksa12@gmail.com>
2021-02-08 20:14:06 +0700

    fix promise error

--- public/worker.js
+++ public/worker.js
@@ -13,14 +13,28 @@ self.addEventListener("install", (event) => {
 });
 
 // Cache and return requests
-self.addEventListener("fetch", (event) => {
+addEventListener("fetch", function (event) {
   event.respondWith(
     caches.match(event.request).then(function (response) {
-      // Cache hit - return response
       if (response) {
-        return response;
+        return response; // if valid response is found in cache return it
+      } else {
+        return fetch(event.request) //fetch from internet
+          .then(function (res) {
+            return caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
+              cache.put(event.request.url, res.clone()); //save the response for future
+              return res; // return the fetched data
+            });
+          })
+          .catch(function (err) {
+            // fallback mechanism
+            return caches
+              .open(CACHE_CONTAINING_ERROR_MESSAGES)
+              .then(function (cache) {
+                return cache.match("/offline.html");
+              });
+          });
       }
-      return fetch(event.request);
     })
   );
 });
